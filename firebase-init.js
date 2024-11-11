a3a98e0227948d98528412dfa2b6eb7313524616
brightgu <88411212+brightgu9@users.noreply.github.com>
2024-11-11 11:04:52 +0900

    modified:   firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,14 +1,16 @@
-// Firebase 설정 정보
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
+document.addEventListener("DOMContentLoaded", function() {
+    // Firebase 설정 정보
+    const firebaseConfig = {
+      apiKey: "YOUR_API_KEY",
+      authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+      projectId: "YOUR_PROJECT_ID",
+      storageBucket: "YOUR_PROJECT_ID.appspot.com",
+      messagingSenderId: "YOUR_SENDER_ID",
+      appId: "YOUR_APP_ID"
+    };
   
-  // Firebase 초기화
-  firebase.initializeApp(firebaseConfig);
-  console.log("Firebase 연결 성공");
+    // Firebase 초기화
+    firebase.initializeApp(firebaseConfig);
+    console.log("Firebase 연결 성공");
+  });
   
